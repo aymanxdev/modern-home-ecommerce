3dcc471b56d66d0ff3fc5f7b0ae951e7260f185a
aymanxdev <ayman.bacc@gmail.com>
2022-01-10 20:48:34 +0000

    add admin page initial ui and setup

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./topbar.styles.css";
-import { NotificationsNoneOutlined } from "@mui/icons-material";
+import {
+  NotificationsNoneOutlined,
+  GTranslateOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
 const Topbar = () => {
   return (
     <div className="topbar-container">
@@ -9,9 +13,23 @@ const Topbar = () => {
           <span className="logo">Tiny Home Admin</span>
         </div>
         <div className="topbar-right">
-          <div className="topbar-icons-container">
-            <NotificationsNoneOutlined />
-            <span className="topIcon-batch">2</span>
+          <div className="right-icons-container">
+            <div className="topbar-icon-container">
+              <NotificationsNoneOutlined />
+              <span className="topIcon-badge">2</span>
+            </div>
+            <div className="topbar-icon-container">
+              <GTranslateOutlined />
+              <span className="topIcon-badge">2</span>
+            </div>
+            <div className="topbar-icon-container">
+              <SettingsOutlined />
+            </div>
+            <img
+              src="https://bit.ly/3GqErl2"
+              alt="avatar"
+              className="topAvatar"
+            />
           </div>
         </div>
       </div>
