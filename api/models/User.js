a873a309eab7511219eebec8ca87922650af005d
aymanxdev <ayman.bacc@gmail.com>
2022-01-22 21:49:36 +0000

    update the user model to include a bit more data

--- api/models/User.js
+++ api/models/User.js
@@ -5,6 +5,10 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, unique: true },
+    Name: { type: String, default: "Not Provided" },
+    phone: { type: String, default: "N/A" },
+    address: { type: String, default: "N/A" },
+    status: { type: Boolean, default: true },
     isAdmin: {
       type: Boolean,
       default: false,
